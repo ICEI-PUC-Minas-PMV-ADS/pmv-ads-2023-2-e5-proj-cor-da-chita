88a4ca78cef48e0f247a72594b23b8248a94748e
54rtor1 <1929sartori@gmail.com>
2023-10-11 16:48:20 +0200

    banner rendering on landing page

--- src/cor-da-chita-app/app/page.tsx
+++ src/cor-da-chita-app/app/page.tsx
@@ -2,20 +2,22 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Banner from "../components/Banner";
+
 
 export default function Home() {
   const route = useRouter();
   return (
     <main>
-      <p className="text-2xl font-extrabold  tracking-tight md:leading-14 text-center">
-        LANDING PAGE AQUI
+      <div>
+        <Banner />
         <br />
-        <Link href="/all-products">Ir para produtos</Link>        
+        <Link href="/all-products">Ir para produtos</Link>
         <button
           type="button"
           onClick={() => route.push("/all-products")}
         ></button>
-      </p>
+      </div>
     </main>
   );
 }
