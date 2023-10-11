7959f63d04ef484f46e2eca0d0c986ffb06c398b
54rtor1 <1929sartori@gmail.com>
2023-10-11 16:48:20 +0200

    banner rendering on landing page

--- src/cor-da-chita-app/components/Banner.tsx
+++ src/cor-da-chita-app/components/Banner.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Banner(...props: any) {
-  return <></>;
+
+export default function Banner() {
+  return (
+    <div className="relative h-screen flex items-center justify-center bg-gray-800">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      <div className="text-center z-10">
+        <h1 className="text-4xl font-bold mb-4">Cor da Chita</h1>
+      </div>
+
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8">
+        <p>image</p>
+        {/* <Image src={imageUrl} alt="Banner Image" width={400} height={300} /> */}
+      </div>
+    </div>
+  );
 }
