2b1cc6ed49c747b94fe207f099af6374a5555d8c
54rtor1 <1929sartori@gmail.com>
2023-10-12 14:11:48 +0200

    sets customized button

--- src/cor-da-chita-app/components/Banner.tsx
+++ src/cor-da-chita-app/components/Banner.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Image from 'next/image';
+import { MyButton } from "./ui/Button.tsx";
 
 
 export default function Banner() {
   return (
-    <div className="relative h-screen flex items-center justify-center bg-black">
-      <div className="text-center z-10">
-        <h1 className="text-4xl font-bold mb-4">Cor da Chita</h1>
+    <div className="relative h-screen flex justify-center bg-black">
+      <div className="text-center z-10 pt-20">
+        <h1 className="text-4xl font-bold text-white">Arte com Chita & Cia para alegrar a vida o ano todo</h1>
+        <div>
+      <div className="pt-20">
+      <MyButton>
+        Ver todos os produtos
+      </MyButton>
       </div>
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8">
+        </div>
+      </div>
+      <div className="absolute bottom-0">
+      <div className="banner-img-wrapper"
+          style={{
+            width: '600px',
+            height: '300px',
+            overflow: 'hidden',
+          }}
+        >
         <Image src="/imgs/chita.jpeg" alt="Banner Image" width={600} height={300} />
+        </div>
       </div>
     </div>
   );
