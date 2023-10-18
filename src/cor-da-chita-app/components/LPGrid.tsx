2720c19315f9daadf63e11a5b61d8df28de03576
54rtor1 <1929sartori@gmail.com>
2023-10-18 15:58:27 +0200

    adds centralized img andtransparent variable button

--- src/cor-da-chita-app/components/LPGrid.tsx
+++ src/cor-da-chita-app/components/LPGrid.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
+import { MyButton } from "./ui/Button.tsx";
+
+
 const LPGrid = () => {
   return (
     <div className="bg-black">
       <div className="grid grid-cols-2">
-        <div className="grid-box bg-green">
-            <p className="text-white">Coluna 1</p>
+        <div className="grid-box bg-green flex items-center justify-center">
+          <div className="grid-img-wrapper bg-white p-4">
+            <Image src="/imgs/chita.jpeg" alt="Banner Image" layout="fill" objectFit="cover" />
+          </div>
         </div>
-        <div className="grid-box bg-light">
-            <p className="text-white">Coluna 2</p>
+        <div className="grid-box bg-light flex flex-col items-center justify-center">
+          <p className="text-white mb-4">Coluna 2</p>
+          <MyButton color="transparent">
+            Ver produtos
+          </MyButton>
         </div>
       </div>
     </div>
