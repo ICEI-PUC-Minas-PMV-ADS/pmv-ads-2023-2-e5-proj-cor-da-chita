d14fa3d95932d7523473483c022cc1de51625cdf
54rtor1 <1929sartori@gmail.com>
2023-10-23 13:08:54 +0200

    git rebase

--- src/cor-da-chita-app/components/Banner.tsx
+++ src/cor-da-chita-app/components/Banner.tsx
@@ -1,18 +1,15 @@
 import React from "react";
+import Image from 'next/image';
 
 
 export default function Banner() {
   return (
-    <div className="relative h-screen flex items-center justify-center bg-gray-800">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-
+    <div className="relative h-screen flex items-center justify-center bg-black">
       <div className="text-center z-10">
         <h1 className="text-4xl font-bold mb-4">Cor da Chita</h1>
       </div>
-
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-8">
-        <p>image</p>
-        {/* <Image src={imageUrl} alt="Banner Image" width={400} height={300} /> */}
+        <Image src="/imgs/chita.jpeg" alt="Banner Image" width={600} height={300} />
       </div>
     </div>
   );
