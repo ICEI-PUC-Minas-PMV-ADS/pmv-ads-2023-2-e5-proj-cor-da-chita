51394e97bf9676c1f83aad0f086d86305ae4b4fd
54rtor1 <1929sartori@gmail.com>
2023-10-23 14:55:08 +0200

    footer OK

--- src/cor-da-chita-app/components/Footer.tsx
+++ src/cor-da-chita-app/components/Footer.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import LinkWhatsApp from "./links/LinkWhatsApp";
 import LinkInstagram from "./links/LinkInstagram";
 
+
 export default function Footer({ children, ...props }: any) {
   return (
     <footer {...props}>
-      <div className="grid grid-cols-2  p-5">
+      <div className="grid grid-cols-2  p-5 bg-green text-light text-tiny">
       <div className="p-5">
         <h3>
           <strong>COR DA CHITA</strong>
         </h3>
         <br/>
-        <p>Feito em João Pessoa, Paraíba</p>
-        <p>Por Madriana Nóbrega</p>
-        <p>&copy; 2020-2023</p>
+        <p className="py-1">Feito em João Pessoa, Paraíba</p>
+        <p className="py-1">Por Madriana Nóbrega</p>
+        <p className="py-1">&copy; 2020-2023</p>
       </div>
       <div  className="grid grid-cols-2">
         <div className="place-content-center p-5">
@@ -21,8 +22,8 @@ export default function Footer({ children, ...props }: any) {
           <strong>INFORMAÇÃO</strong>
         </h4>
         <br/>
-        <p>Sobre Cor da Chita</p>
-        <p>Perguntas Frequentes</p>
+        <p className="text-tiny py-1">Sobre Cor da Chita</p>
+        <p className="text-tiny py-1">Perguntas Frequentes</p>
         </div>
         <div className="place-content-center p-5">
           <h4 >
