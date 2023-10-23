e413e18e7996a4a92561f900e0e2191b6042e950
54rtor1 <1929sartori@gmail.com>
2023-10-23 13:27:45 +0200

    makes footer components in cols

--- src/cor-da-chita-app/components/Footer.tsx
+++ src/cor-da-chita-app/components/Footer.tsx
@@ -7,6 +7,7 @@ import LinkInstagram from "./links/LinkInstagram";
 export default function Footer({ children, ...props }: any) {
   return (
     <footer {...props}>
+      <div className="grid grid-cols-2">
       <div>
         <h3>
           <strong>COR DA CHITA</strong>
@@ -15,13 +16,13 @@ export default function Footer({ children, ...props }: any) {
         <p>Por Madriana Nóbrega</p>
         <p>&copy; 2020-2023</p>
       </div>
-      <div>
+      <div  className="grid grid-cols-2">
         <h4>
           <strong>INFORMAÇÃO</strong>
           <p>Sobre Cor da Chita</p>
           <p>Perguntas Frequentes</p>
         </h4>
-        <div>
+        <div >
           <h4>
             <strong>CONTATO</strong>
             <div>
@@ -30,6 +31,7 @@ export default function Footer({ children, ...props }: any) {
             </div>
           </h4>
         </div>
+      </div>
       </div>
     </footer>
   );
