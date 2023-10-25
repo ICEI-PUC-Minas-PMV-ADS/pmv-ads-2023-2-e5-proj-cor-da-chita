502ede8250c05cc90e3712702b33b846fc2d32f9
54rtor1 <1929sartori@gmail.com>
2023-10-25 21:23:58 +0200

    adds two more layers to lp grid

--- src/cor-da-chita-app/components/LPGrid.tsx
+++ src/cor-da-chita-app/components/LPGrid.tsx
@@ -20,6 +20,35 @@ const LPGrid = () => {
           </MyButton>
           </div>
         </div>
+        <div className="grid-box bg-light flex flex-col items-center justify-center">
+          <div className="elements text-left p-20">
+            <p className="uppercase">Cama e Mesa</p>
+            <p className="py-8 font-thin">Toalhas, passadeiras, colchas e jogo americano dupla face em retalhos. Tudo feito de chita 100% algodão. </p>
+            <MyButton color="transparent" className="py-8">
+              Ver produtos
+            </MyButton>
+          </div>
+        </div>
+        <div className="grid-box bg-green flex items-center justify-center">
+          <div className="grid-img-wrapper bg-white p-4">
+            <Image src="/imgs/chita.jpeg" alt="Banner Image" layout="fill" objectFit="cover" />
+          </div>
+        </div>
+        <div className="grid-box bg-green flex items-center justify-center">
+          <div className="grid-img-wrapper bg-white p-4">
+            <Image src="/imgs/chita.jpeg" alt="Banner Image" layout="fill" objectFit="cover" />
+          </div>
+        </div>
+        <div className="grid-box bg-light flex flex-col items-center justify-center">
+          <div className="elements text-left p-15">
+          <p className="uppercase">Ecobags e Carteiras</p>
+          <p className="py-8 font-thin">Com tamanho pra todo agrado, com zíper e bolsos interno e externo. </p>
+          <MyButton color="transparent" className="py-8">
+            Ver produtos
+          </MyButton>
+          </div>
+        </div>
+
       </div>
     </div>
   );
