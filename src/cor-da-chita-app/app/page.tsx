956d33d8591652a089e968563b07a53615db84d3
Julia Sartori <1929sartori@gmail.com>
2023-11-07 21:29:07 +0100

    wip insta connection

--- src/cor-da-chita-app/app/page.tsx
+++ src/cor-da-chita-app/app/page.tsx
@@ -5,19 +5,21 @@ import React from "react";
 import Banner from "../components/Banner";
 import LPGrid from "../components/LPGrid";
 import QuotesTabs from "../components/Tabs";
+import Instagram from "../components/Instagram";
 
 export default function Home() {
   const route = useRouter();
   return (
     <main className="bg-light">
       <Banner />
+      <QuotesTabs />
       <LPGrid />
+      <Instagram />
       <div className="bg-dark  text-center">
         <Link href="/all-products">
           <h2 className="text-3xl text-white p-20 ">Ver todos os produtos</h2>
         </Link>
       </div>
-      <QuotesTabs />
     </main>
   );
 }
---
+++ src/cor-da-chita-app/components/Instagram.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Image } from '@nextui-org/react';
+import Link from 'next/link';
+
+export default function Instagram() {
+  const [feed, setFeed] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `https://graph.instagram.com/v18.0/me?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.INSTAGRAM_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setFeed(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching Instagram data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures that this effect runs once when the component mounts
+
+  return (
+    <div className="flex justify-center bg-light">
+      <Link href="/">oi</Link>
+      <Image src="" alt="" />
+    </div>
+  );
+}
