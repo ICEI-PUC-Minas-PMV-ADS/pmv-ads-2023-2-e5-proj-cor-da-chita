51577c8093fd7f2902ab9b5f67ee1964b464a498
Julia Sartori <1929sartori@gmail.com>
2023-11-07 13:43:37 +0100

    styles tabs

--- src/cor-da-chita-app/components/Tabs.tsx
+++ src/cor-da-chita-app/components/Tabs.tsx
@@ -3,8 +3,8 @@ import { Tabs, Tab } from "@nextui-org/react";
 
 export default function QuotesTabs() {
   return (
-    <div className="flex flex-col bg-dark text-white p-10 items-center">
-      <Tabs aria-label="Options" radius="none" size="lg" variant="underlined">
+    <div className="flex flex-col bg-light p-10 items-center">
+      <Tabs aria-label="Options" radius="none" size="lg" variant="underlined" classNames={{tabList: "gap-6 w-full relative rounded-none p-0 border-b border-divider"}}>
         <Tab key="1" title={<SquareIcon />} className="flex flex-col items-center">
           <div className="flex-1 text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -17,15 +17,16 @@ export default function QuotesTabs() {
         </Tab>
         <Tab key="3" title={<SquareIcon />} className="flex flex-col items-center">
           <div className="flex-1 text-center">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </div>
+            <blockquote className="text-2xl font-light tracking-wide italic">
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </blockquote>
+            </div>
         </Tab>
       </Tabs>
     </div>
   );
 }
 
-// Function to generate a square icon (you can customize the size and color)
 const SquareIcon = () => (
-  <div className="w-10 h-10 bg-white"></div>
+  <div className="m-3 w-10 h-5 bg-green"></div>
 );
