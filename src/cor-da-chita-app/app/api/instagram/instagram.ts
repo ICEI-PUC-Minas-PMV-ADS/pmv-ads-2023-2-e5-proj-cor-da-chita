ddc808e75adc9e3c41930ce20f616d226bbe5e18
Julia Sartori <1929sartori@gmail.com>
2023-11-08 17:15:50 +0100

    public next api key - temporary solution

--- src/cor-da-chita-app/app/api/instagram/instagram.ts
+++ src/cor-da-chita-app/app/api/instagram/instagram.ts
@@ -1,6 +1,6 @@
 export default async function GetInstaData() {
   try {
-    const url = `https://graph.instagram.com/v12.0/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.INSTAGRAM_KEY}`;
+    const url = `https://graph.instagram.com/v12.0/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
