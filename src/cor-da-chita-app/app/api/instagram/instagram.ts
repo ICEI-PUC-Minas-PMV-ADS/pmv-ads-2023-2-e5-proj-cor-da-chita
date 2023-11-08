cc6e6a50d86273e03515a2e685834d9b8422103e
Julia Sartori <1929sartori@gmail.com>
2023-11-08 17:00:51 +0100

    still only works w direct token

---
+++ src/cor-da-chita-app/app/api/instagram/instagram.ts
@@ -0,0 +1,12 @@
+export default async function GetInstaData() {
+  try {
+    const url = `https://graph.instagram.com/v12.0/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.INSTAGRAM_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data; 
+  } catch (error) {
+    console.error('Error fetching Instagram data:', error);
+    throw error; 
+  }
+}
