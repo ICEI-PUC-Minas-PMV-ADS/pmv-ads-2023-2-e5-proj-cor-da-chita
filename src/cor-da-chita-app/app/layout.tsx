7fb0168cacc74bfa92cda610e2a3b1aaa98e6a57
Ana Paula Buchholz <anapbuchholz@gmail.com>
2023-11-07 21:09:19 -0300

    [feature] Implementando conexão com google analytics

--- src/cor-da-chita-app/app/layout.tsx
+++ src/cor-da-chita-app/app/layout.tsx
@@ -5,6 +5,7 @@ import Menu from "../components/Menu";
 import Footer from "../components/Footer";
 
 import { Providers } from "./api/providers/Providers";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -15,6 +16,21 @@ export default function RootLayout({
     <html lang="pt-br">
       <body suppressHydrationWarning={true}>
         <Providers>
+        <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-19VJCQS2H4`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-19VJCQS2H4', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
           <NavBar />
           <Menu />
           {children}
