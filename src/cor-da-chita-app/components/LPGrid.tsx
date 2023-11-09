c3c524431c54332d3015547193023f35f1cbd8e5
Julia Sartori <1929sartori@gmail.com>
2023-11-10 00:22:47 +0100

    fixes grid

--- src/cor-da-chita-app/components/LPGrid.tsx
+++ src/cor-da-chita-app/components/LPGrid.tsx
@@ -130,39 +130,9 @@ const LPGrid = () => {
           </div>
         </div>
 
-        {/* Second row */}
-        <div className="grid-box bg-white flex items-center justify-center">
-          <div className="elements text-left p-15">
-            <p className="uppercase">Your Title</p>
-            <p className="py-8 font-thin">Your description here.</p>
-            <Link href="/all-products">
-              <MyButton color="transparent" className="py-8">
-                Ver produtos
-              </MyButton>
-            </Link>
-          </div>
-        </div>
-        <div className="grid-box bg-green flex items-center justify-center">
-          <div className="grid-img-wrapper bg-white p-4">
-            {/* Your content for the second box in the second row */}
-          </div>
-        </div>
-        <div className="grid-box bg-white flex items-center justify-center">
-          <div className="elements text-left p-15">
-            <p className="uppercase">Your Title</p>
-            <p className="py-8 font-thin">Your description here.</p>
-            <Link href="/all-products">
-              <MyButton color="transparent" className="py-8">
-                Ver produtos
-              </MyButton>
-            </Link>
-          </div>
-        </div>
       </div>
     </div>
   );
 };
-
-
 
 export default LPGrid;
