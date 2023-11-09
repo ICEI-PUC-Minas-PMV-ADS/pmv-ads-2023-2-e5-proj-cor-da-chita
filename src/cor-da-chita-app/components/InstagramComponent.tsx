d912705eebbe081128b4bfc2c8a494d2f482ce72
Julia Sartori <1929sartori@gmail.com>
2023-11-09 20:19:47 +0100

    instagram link added

--- src/cor-da-chita-app/components/InstagramComponent.tsx
+++ src/cor-da-chita-app/components/InstagramComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image } from '@nextui-org/react';
+import { Image, Link } from '@nextui-org/react';
 
 const InstagramComponent: React.FC = () => {
   const [instagramData, setInstagramData] = useState([]);
@@ -19,20 +19,26 @@ const InstagramComponent: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-full">
-      <div className="flex flex-auto justify-around space-x-4">
+    <div className="w-full bg-light py-10">
+      <div className="text-center py-8">
+          <h2>siga o Cor da Chita no Instagram!</h2>
+          <Link   isExternal
+           aria-label="Instagram Cor da Chita"
+            href="https://www.instagram.com/cor.da.chita/"><p className="font-semibold">@cor.da.chita</p></Link>
+      </div>
+      <div className="flex flex-auto justify-around space-x-4 px-10">
         {instagramData.map((item) => (
           <a
             key={item.id}
             href={item.permalink}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex flex-col items-center text-center"
+            className="flex flex-col"
           >
             <div
               style={{
-                width: '300px', // Adjust the width as needed
-                height: '300px', // Fixed height for the square
+                width: '300px', 
+                height: '300px', 
                 overflow: 'hidden',
               }}
             >
@@ -46,8 +52,8 @@ const InstagramComponent: React.FC = () => {
               />
             </div>
             <div className="mt-2">
-              <p className="font-semibold">{item.username}</p>
-              <p className="text-sm">{item.caption}</p>
+              <span><p className="font-semibold text-tiny">{item.username}</p></span>
+              <p className="text-sm text-left">{item.caption}</p>
             </div>
           </a>
         ))}
