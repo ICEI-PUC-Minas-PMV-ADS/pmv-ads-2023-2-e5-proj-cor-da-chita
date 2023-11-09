741752d256a5ed0167b9e950efe01d445ffd4dec
Julia Sartori <1929sartori@gmail.com>
2023-11-10 00:14:01 +0100

    lp v1 ok

--- src/cor-da-chita-app/components/LPGrid.tsx
+++ src/cor-da-chita-app/components/LPGrid.tsx
@@ -1,15 +1,28 @@
-import Image from 'next/image';
 import { MyButton } from "./ui/Button.tsx";
-import Link from "next/link";
+import { Image, Link } from '@nextui-org/react';
 
 const LPGrid = () => {
   return (
     <div className="bg-black">
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-4">
         {/* First row */}
         <div className="grid-box bg-green flex items-center justify-center">
-          <div className="grid-img-wrapper bg-white p-4">
-            {/* Your content for the first box in the first row */}
+          <div className="grid-img-wrapper bg-white">
+            <div
+              style={{
+                width: '300px', 
+                height: '300px', 
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                src="/Chita/IMG_6812 (1).png"
+                width="300"
+                height="300"
+                layout="responsive"
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            </div>
           </div>
         </div>
         <div className="grid-box bg-white flex items-center justify-center">
@@ -24,31 +37,70 @@ const LPGrid = () => {
           </div>
         </div>
         <div className="grid-box bg-green flex items-center justify-center">
-          <div className="grid-img-wrapper bg-white p-4">
-            {/* Your content for the third box in the first row */}
+          <div className="grid-img-wrapper bg-white">
+            <div
+              style={{
+                width: '300px', 
+                height: '300px', 
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                src="/Chita/IMG_6813 (1).png"
+                width="300"
+                height="300"
+                layout="responsive"
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="grid-box bg-white flex items-center justify-center">
+          <div className="elements text-left p-15">
+            <p className="uppercase text-lg">Your Title</p>
+            <p className="py-8 font-thin">Your description here.</p>
+            <Link href="/all-products">
+              <MyButton color="transparent" className="py-8">
+                Ver produtos
+              </MyButton>
+            </Link>
+          </div>
+        </div>
+        <div className="grid-box bg-white flex items-center justify-center">
+          <div className="elements text-left p-15">
+            <p className="uppercase text-lg">Your Title</p>
+            <p className="py-8 font-thin">Your description here.</p>
+            <Link href="/all-products">
+              <MyButton color="transparent" className="py-8">
+                Ver produtos
+              </MyButton>
+            </Link>
           </div>
         </div>
 
-        {/* Second row */}
-        <div className="grid-box bg-white flex items-center justify-center">
-          <div className="elements text-left p-15">
-            <p className="uppercase">Your Title</p>
-            <p className="py-8 font-thin">Your description here.</p>
-            <Link href="/all-products">
-              <MyButton color="transparent" className="py-8">
-                Ver produtos
-              </MyButton>
-            </Link>
-          </div>
-        </div>
         <div className="grid-box bg-green flex items-center justify-center">
-          <div className="grid-img-wrapper bg-white p-4">
-            {/* Your content for the second box in the second row */}
+          <div className="grid-img-wrapper bg-white">
+            <div
+              style={{
+                width: '300px', 
+                height: '300px', 
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                src="/Chita/IMG_6814 (1).png"
+                width="300"
+                height="300"
+                layout="responsive"
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            </div>
           </div>
         </div>
+
         <div className="grid-box bg-white flex items-center justify-center">
           <div className="elements text-left p-15">
-            <p className="uppercase">Your Title</p>
+            <p className="uppercase text-lg">Your Title</p>
             <p className="py-8 font-thin">Your description here.</p>
             <Link href="/all-products">
               <MyButton color="transparent" className="py-8">
@@ -57,11 +109,30 @@ const LPGrid = () => {
             </Link>
           </div>
         </div>
+
+        <div className="grid-box bg-green flex items-center justify-center">
+          <div className="grid-img-wrapper bg-white">
+            <div
+              style={{
+                width: '300px', 
+                height: '300px', 
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                src="/Chita/IMG_6811 (1).png"
+                width="300"
+                height="300"
+                layout="responsive"
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
 };
-
-
 
 export default LPGrid;
