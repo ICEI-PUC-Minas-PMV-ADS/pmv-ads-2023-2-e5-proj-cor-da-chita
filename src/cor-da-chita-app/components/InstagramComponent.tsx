37df153f25d240c807f1a49c1e68d3c9e1e06b1b
Julia Sartori <1929sartori@gmail.com>
2023-11-12 21:05:27 +0100

    fixes instagram bug

--- src/cor-da-chita-app/components/InstagramComponent.tsx
+++ src/cor-da-chita-app/components/InstagramComponent.tsx
@@ -9,7 +9,9 @@ const InstagramComponent: React.FC = () => {
       try {
         const response = await fetch('/api/instagram');
         const data = await response.json();
-        setInstagramData(data.data.slice(0, 3)); // Take the first three items
+        // Filter out items without URLs (assuming video media items don't have URLs)
+        const filteredData = data.data.filter((item) => item.media_type !== 'VIDEO');
+        setInstagramData(filteredData.slice(0, 3)); // Take the first three image items
       } catch (error) {
         console.error('Error fetching Instagram data:', error);
       }
@@ -21,11 +23,15 @@ const InstagramComponent: React.FC = () => {
   return (
     <div className="w-full bg-light py-10">
       <div className="text-center py-8">
-          <h2>Siga o Cor da Chita no Instagram!</h2>
-          <Link   isExternal
-           aria-label="Instagram Cor da Chita"
-           color="foreground"
-            href="https://www.instagram.com/cor.da.chita/"><p className="font-semibold">@cor.da.chita</p></Link>
+        <h2>Siga o Cor da Chita no Instagram!</h2>
+        <Link
+          isExternal
+          aria-label="Instagram Cor da Chita"
+          color="foreground"
+          href="https://www.instagram.com/cor.da.chita/"
+        >
+          <p className="font-semibold text-small">@Cor.Da.Chita</p>
+        </Link>
       </div>
       <div className="flex flex-auto justify-around space-x-4 px-10">
         {instagramData.map((item) => (
@@ -35,11 +41,12 @@ const InstagramComponent: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
             className="flex flex-col"
+            style={{ maxWidth: '300px' }}
           >
             <div
               style={{
-                width: '300px', 
-                height: '300px', 
+                width: '300px',
+                height: '300px',
                 overflow: 'hidden',
               }}
             >
@@ -53,8 +60,8 @@ const InstagramComponent: React.FC = () => {
               />
             </div>
             <div className="mt-2">
-              <span><p className="font-semibold text-tiny">{item.username}</p></span>
-              <p className="text-sm text-left">{item.caption}</p>
+              <span><p className="font-semibold capitalize text-small">{item.username}</p></span>
+              <p className="text-sm text-left overflow-hidden overflow-ellipsis line-clamp-1">{item.caption}</p>
             </div>
           </a>
         ))}
