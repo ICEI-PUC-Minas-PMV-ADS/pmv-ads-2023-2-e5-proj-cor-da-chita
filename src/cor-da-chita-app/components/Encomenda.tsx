a5a0e76f9f888360951eb0c6c765e959b75d8a06
Julia Sartori <1929sartori@gmail.com>
2023-11-18 17:04:45 +0100

    encomenda

--- src/cor-da-chita-app/components/Encomenda.tsx
+++ src/cor-da-chita-app/components/Encomenda.tsx
@@ -1,21 +1,34 @@
-import Link from "next/link";
+import { Link } from "@nextui-org/react";
 import { MyButton } from "@/components/ui/Button";
 
 export default function Encomenda() {
-    return (
-        <div className="bg-green  flex justify-around p-20 px-20">
-        <Link href="/all-products">
-          <h2 className="text-3xl text-white font-serif text-semibold py-2 ">
-            Quer um produto Cor da Chita personalizado?
-          </h2>
-          <h3 className="text-2xl text-light text-white">
-            Faça sua encomenda aqui
-          </h3>
-        </Link>
-        <MyButton color="transparent" className="px-10 text-light">
-          Entre em contato aqui
-        </MyButton>
+  const handleContactButtonClick = () => {
+    const emailAddress = 'your-email@example.com';
+    
+    window.location.href = `mailto:${emailAddress}`;
+  };
+
+  return (
+    <div className="bg-green  flex justify-between px-20 py-5">
+      <div>
+      <h2 className="text-2xl text-white font-serif text-semibold py-2 ">
+          Quer um produto Cor da Chita personalizado?
+        </h2>
+        <h3 className="text-xl text-light text-white">
+          Faça sua encomenda!
+        </h3>
       </div>
-    );
-  }
-  
+      
+      <MyButton
+        color="transparent"
+        className="px-10 text-light"
+      >
+      <Link 
+       isExternal
+       className="text-white"
+       href="https://api.whatsapp.com/send?phone=5583987261972"
+      >Entre em contato aqui</Link>
+      </MyButton>
+    </div>
+  );
+}
