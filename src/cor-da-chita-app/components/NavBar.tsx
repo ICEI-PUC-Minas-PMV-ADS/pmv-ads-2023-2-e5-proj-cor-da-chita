175f05953f773772e2406b943266606482258668
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-11-19 14:31:24 -0300

    teste pedido

--- src/cor-da-chita-app/components/NavBar.tsx
+++ src/cor-da-chita-app/components/NavBar.tsx
@@ -7,10 +7,7 @@ import CartIcon from "@/assets/icons/CartIcon";
 import OrdersIcon from "@/assets/icons/OrdersIcon";
 import HelpIcon from "@/assets/icons/HelpIcon";
 import FlowerIcon from "@/assets/icons/FlowerIcon";
-import TotalCartItems from "@/components/ui/TotalCartItems"; 
-
-
-
+import TotalCartItems from "@/components/ui/TotalCartItems";
 
 import {
   Navbar,
@@ -44,72 +41,100 @@ export default function NavBar() {
   };
 
   return (
-    <Navbar shouldHideOnScroll maxWidth="full" isBordered className="bg-light flex justify-between">
-       <div className="flex items-center">
-       <NavbarBrand onClick={handleClick} className="bg-light ">
-        <CorChitaFlor />
-        <CorChitaTexto />
-      </NavbarBrand>
-       </div> 
-       <div className="flex items-center">
-      <NavbarContent>
-         <SearchInput/>
-         <NavbarItem>
-            <TotalCartItems /> 
+    <Navbar
+      shouldHideOnScroll
+      maxWidth="full"
+      isBordered
+      className="bg-light flex justify-between"
+    >
+      <div className="flex items-center">
+        <NavbarBrand onClick={handleClick} className="bg-light ">
+          <CorChitaFlor />
+          <CorChitaTexto />
+        </NavbarBrand>
+      </div>
+      <div className="flex items-center">
+        <NavbarContent>
+          <SearchInput />
+          <NavbarItem>
+            <TotalCartItems />
           </NavbarItem>
-        <NavbarItem>
-        {session && session.user ? (
-          <Dropdown className="p-0 rounded-md shadow-none">
-            <DropdownTrigger>
-              <Avatar
-                as="button"
-                className="transition-transform"
-                name={session.user.name ?? ""}
-                src={session.user.image ?? ""}
-              />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Profile Actions" variant="bordered"  className="bg-light" color="success" disabledKeys={[""]}>
-              <DropdownItem key="profile" className="text-end" color="default">
-                <p className="font-semibold text-tiny">{session.user.name}</p>
-              </DropdownItem>
-              <DropdownItem
-                key="my_Cart"
-                onClick={() => route.push("/shop-cart")}
-                endContent={<CartIcon className={iconClasses} />}
-              ><p className="text-tiny p-4">
-                Carrinho
-                </p>
-              </DropdownItem>
-              <DropdownItem key="my_orders" onClick={()=>route.push("/my-orders")} endContent={<OrdersIcon className={iconClasses} />}><p className="text-tiny p-4">Meus Pedidos</p></DropdownItem>
-              <DropdownItem key="help" endContent={<HelpIcon className={iconClasses}/>}><p className="text-tiny p-4" onClick={() => route.push("/question")}>Perguntas Frequentes</p></DropdownItem>
-              <DropdownItem
-                key="about"
-                onClick={() => route.push("/about")}
-                endContent={<FlowerIcon className={iconClasses} />}
-              ><p className="text-tiny p-4">
-                Sobre o Cor da Chita
-                </p>
-                </DropdownItem>
-              <DropdownItem
-                key="logout"
-                color="danger"
-                onClick={() => signOut()}
-              ><p className="text-tiny p-1">
-                Sair</p>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        ) : (
-          <Button variant="ghost" onClick={() => signIn("google")}>
-            Entrar
-          </Button>
-        )}
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarItem>
+            {session && session.user ? (
+              <Dropdown className="p-0 rounded-md shadow-none">
+                <DropdownTrigger>
+                  <Avatar
+                    as="button"
+                    className="transition-transform"
+                    name={session.user.name ?? ""}
+                    src={session.user.image ?? ""}
+                  />
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label="Profile Actions"
+                  variant="bordered"
+                  className="bg-light"
+                  color="success"
+                  disabledKeys={[""]}
+                >
+                  <DropdownItem
+                    key="profile"
+                    className="text-end"
+                    color="default"
+                  >
+                    <p className="font-semibold text-tiny">
+                      {session.user.name}
+                    </p>
+                  </DropdownItem>
+                  <DropdownItem
+                    key="my_Cart"
+                    onClick={() => route.push("/shop-cart")}
+                    endContent={<CartIcon className={iconClasses} />}
+                  >
+                    <p className="text-tiny p-4">Carrinho</p>
+                  </DropdownItem>
+                  <DropdownItem
+                    key="my_orders"
+                    onClick={() => route.push("/my-orders")}
+                    endContent={<OrdersIcon className={iconClasses} />}
+                  >
+                    <p className="text-tiny p-4">Meus Pedidos</p>
+                  </DropdownItem>
+                  <DropdownItem
+                    key="help"
+                    endContent={<HelpIcon className={iconClasses} />}
+                  >
+                    <p
+                      className="text-tiny p-4"
+                      onClick={() => route.push("/question")}
+                    >
+                      Perguntas Frequentes
+                    </p>
+                  </DropdownItem>
+                  <DropdownItem
+                    key="about"
+                    onClick={() => route.push("/about")}
+                    endContent={<FlowerIcon className={iconClasses} />}
+                  >
+                    <p className="text-tiny p-4">Sobre o Cor da Chita</p>
+                  </DropdownItem>
+                  <DropdownItem
+                    key="logout"
+                    color="danger"
+                    onClick={() => signOut()}
+                  >
+                    <p className="text-tiny p-1">Sair</p>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            ) : (
+              <Button variant="ghost" onClick={() => signIn("google")}>
+                Entrar
+              </Button>
+            )}
+          </NavbarItem>
+        </NavbarContent>
       </div>
     </Navbar>
   );
 }
-
-
-
--- src/cor-da-chita-app/components/ui/TotalCartItems.tsx
+++ src/cor-da-chita-app/components/ui/TotalCartItems.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Button } from "@nextui-org/react";
 import { CartItemsContext } from "@/contexts/CartContext/CartItemsContext";
 import { useRouter } from "next/navigation";
@@ -8,23 +8,26 @@ const TotalCartItems = () => {
   const route = useRouter();
   const { cartItems } = useContext(CartItemsContext);
 
-  const totalQuantity = cartItems.reduce((total, item) => total + item.quantidade, 0);
+  const totalQuantity = cartItems.reduce(
+    (total, item) => total + item.quantidade,
+    0
+  );
 
   return (
-
     <Button
-    isIconOnly
-    variant="ghost"
-    color="success"
-    onClick={() => route.push("/shop-cart")}  >
-     <div className="flex items-center relative pl-1">
-      <CartIcon className="w-4 h-4" />
-      <div className="rounded-full text-xs">
-        <div className="w-4 h-4 flex items-center pb-2 justify-center">
+      isIconOnly
+      variant="ghost"
+      color="success"
+      onClick={() => route.push("/shop-cart")}
+    >
+      <div className="flex items-center relative pl-1">
+        <CartIcon className="w-4 h-4" />
+        <div className="rounded-full text-xs">
+          <div className="w-4 h-4 flex items-center pb-2 justify-center">
             {totalQuantity > 0 ? totalQuantity : 0}
+          </div>
         </div>
       </div>
-    </div>
     </Button>
   );
 };
