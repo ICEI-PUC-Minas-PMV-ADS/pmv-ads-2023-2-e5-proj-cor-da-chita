033357ba300c3375fe0aff0010740408e0b98e65
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-11-19 16:03:43 -0300

    tabela

--- src/cor-da-chita-app/app/(order)/my-orders/page.tsx
+++ src/cor-da-chita-app/app/(order)/my-orders/page.tsx
@@ -7,8 +7,19 @@ import { useSession } from "next-auth/react";
 import { MyButton } from "@/components/ui/Button";
 import ArrowLeft from "@/assets/icons/ArrowLeft";
 
-
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue} from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  User,
+  Chip,
+  Tooltip,
+  ChipProps,
+  getKeyValue,
+} from "@nextui-org/react";
 
 import { url } from "@/app/api/backend/webApiUrl";
 import { UserContext } from "@/contexts/UserContext/UserContext";
@@ -18,37 +29,87 @@ import Cep from "@/app/api/cep/cep";
 import { CepContext } from "@/contexts/CepContext/CepContext";
 import axios from "axios";
 
+interface Order {
+  id: string;
+  items: [
+    {
+      productId: string;
+      productName: string;
+      productPrice: number;
+      productQuantity: number;
+    }
+  ];
+  userName: string;
+  userEmail: string;
+  street: string;
+  neighborhood: string;
+  num: string;
+  city: string;
+  uf: string;
+  cep: string;
+  complement: string;
+  freight: {
+    totalWidthFreight: number;
+    totalHeightFreight: number;
+    totalLengthFreight: number;
+    totalWeightFreight: number;
+    freightValue: number;
+    freightMethod: string;
+  };
+  orderPixId: number;
+  orderDate: string;
+  phoneNumber: string;
+  totalPriceProducts: number;
+}
+
 export default function MyOrders({ ...props }: any) {
   const { data: session } = useSession();
   const route = useRouter();
 
-
   const user = useContext(UserContext);
   const address = useContext(AddressContext);
   const { saveCepContext } = useContext(CepContext); // Se envio por correios
-  const [orders,setOrders] = useState()
+  const [orders, setOrders] = useState(); // Database
+  const [value, setValue] = useState<any>();
+
   const columns = [
-    {name: "NAME", uid: "name"},
-    {name: "ROLE", uid: "role"},
-    {name: "STATUS", uid: "status"},
-    {name: "ACTIONS", uid: "actions"},
+    { name: "NAME", uid: "name" },
+    { name: "ROLE", uid: "role" },
+    { name: "STATUS", uid: "status" },
+    { name: "ACTIONS", uid: "actions" },
   ];
 
-  const getOrderByEmail = async()=>{
+  const getOrderByEmail = async () => {
+    const data = await axios
+      .get(`${url}/Order/OrdersWithEmail/gabrielpuneco@gmail.com`)
+      .then((response) => {
+        return response.data;
+      });
 
-const res = await axios.get(`${url}/Order/OrdersWithEmail/gabrielpuneco@gmail.com`).then(r=>{
-  return r.data
-})
-     
-console.log(res)
+    setOrders(data);
+    console.log(data);
+  };
 
-  }
+  useEffect(() => {
+    getOrderByEmail();
+  }, []);
 
-useEffect(()=>{
+  useEffect(() => {
+    console.log(orders);
 
-getOrderByEmail()
+    if (orders != undefined) {
+      console.log(orders);
 
-},[])
+      const values: any = Object.values(orders);
+      setValue(values);
+
+      values.map((x: any) => console.log(x));
+    }
+  }, [orders]);
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
 
   return (
     <>
@@ -60,31 +121,50 @@ getOrderByEmail()
       >
         <ArrowLeft /> Retornar
       </Link>
-    
       <div className="px-20">
-      
-      <Table aria-label="Example table with custom cells" 
-      removeWrapper	
-      shadow="none"
-      radius="none"
-      className="border"
-      classNames={{ th: 'bg-light border-b-1 text-dark'}}>
-      <TableHeader columns={columns}
-       
-       >
-        {(column) => (
-          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      {orders==undefined ?
-       <TableBody  emptyContent={"Você não possui pedidos no momento"}>{[]}</TableBody>
-      
-      :
-      <TableBody emptyContent={"Você não possui pedidos no momento"}>{[]}</TableBody>
-    }
-    </Table>
+        <Table
+          aria-label="Example table with custom cells"
+          removeWrapper
+          shadow="none"
+          radius="none"
+          className="border"
+          classNames={{ th: "bg-light border-b-1 text-dark" }}
+        >
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn
+                key={column.uid}
+                align={column.uid === "actions" ? "center" : "start"}
+              >
+                {column.name}
+              </TableColumn>
+            )}
+          </TableHeader>
+
+          <TableBody>
+          
+          </TableBody>
+          {/* {orders == undefined ? (
+            <TableBody emptyContent={"Você não possui pedidos no momento"}>
+              {[]}
+            </TableBody>
+          ) : (
+          
+          +
+            <TableBody>
+              {orders.}
+            </TableBody>
+          )} */}
+        </Table>
+        <div>
+          {value ? (
+            value?.map((item: any) => {
+              <p>AQUI:: {item[0]} </p>;
+            })
+          ) : (
+            <>Necas</>
+          )}
+        </div>
       </div>
     </>
   );
