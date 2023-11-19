e557a10eb1d35058c3009b941ddd70df91163490
Julia Sartori <1929sartori@gmail.com>
2023-11-19 14:05:34 +0100

    estiliza tabela

--- src/cor-da-chita-app/app/(order)/my-orders/page.tsx
+++ src/cor-da-chita-app/app/(order)/my-orders/page.tsx
@@ -5,6 +5,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { Button, Input, Link, Spinner } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { MyButton } from "@/components/ui/Button";
+import ArrowLeft from "@/assets/icons/ArrowLeft";
+
 
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue} from "@nextui-org/react";
 
@@ -50,11 +52,26 @@ getOrderByEmail()
 
   return (
     <>
+      <Link
+        size="sm"
+        as="button"
+        className="p-4 my-3 gap-2 tracking-wide text-dark hover:text-success border border-transparent hover:border-success transition-all duration-200"
+        onClick={() => route.back()}
+      >
+        <ArrowLeft /> Retornar
+      </Link>
     
       <div className="px-20">
       
-      <Table aria-label="Example table with custom cells" >
-      <TableHeader columns={columns} >
+      <Table aria-label="Example table with custom cells" 
+      removeWrapper	
+      shadow="none"
+      radius="none"
+      className="border"
+      classNames={{ th: 'bg-light border-b-1 text-dark'}}>
+      <TableHeader columns={columns}
+       
+       >
         {(column) => (
           <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
             {column.name}
