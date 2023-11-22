6a43bc7268f6d566a721a21a5dc5cc38a7204d21
Ana Paula Buchholz <anapbuchholz@gmail.com>
2023-11-21 22:12:47 -0300

    [feature] Inserindo referëncia à variavel que permite publicação com vercel

--- src/cor-da-chita-app/app/api/auth/[...nextauth]/route.ts
+++ src/cor-da-chita-app/app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  secret: process.env.SECRET
   // callbacks: {
   //   async jwt({ token, user }: any) {
   //     if (user) {
