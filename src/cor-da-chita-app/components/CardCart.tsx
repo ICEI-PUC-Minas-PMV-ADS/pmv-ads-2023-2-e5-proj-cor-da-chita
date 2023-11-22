1cffafaceeb03d0a8fa2cd089753d6d6726ec2cf
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-11-22 18:56:11 -0300

    Trocando ponto por virgula no cardCart

--- src/cor-da-chita-app/components/CardCart.tsx
+++ src/cor-da-chita-app/components/CardCart.tsx
@@ -149,7 +149,7 @@ export default function CardCart({ ...props }: any) {
             w-24"
             >
               <h2 className="my-2">{item[0].nome}</h2>
-              <p className="my-2">R$ {item[0].preco.toFixed(2)}</p>
+              <p className="my-2">R$ {item[0].preco.toFixed(2).toString().replace('.',',')}</p>
             </div>
 
             <div className="flex flex-col place-content-evenly">
