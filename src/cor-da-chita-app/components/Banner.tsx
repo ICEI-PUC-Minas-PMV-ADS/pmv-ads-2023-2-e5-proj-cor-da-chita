4d4778284ef9d7fb801a505a41063f7ffa04335b
Julia Sartori <1929sartori@gmail.com>
2023-11-27 15:41:46 +0100

    arrows ok

--- src/cor-da-chita-app/components/Banner.tsx
+++ src/cor-da-chita-app/components/Banner.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image } from "@nextui-org/react";
 import { MyButton } from "./ui/Button";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
+const images = [
+  "Chita (1).png",   "Chita (2).png", "Chita (3).png", 
+];
 
 export default function Banner() {
   const route = useRouter();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex: number) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex: number) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div className="relative h-screen bg-dark flex">
-      <div className="flex-1/2 relative overflow-hidden">
-        <Image
-          src="/Chita/IMG_6811 (1).png"
-          alt="Cor da Chita"
-          style={{ maxWidth: "100%", height: "100%", objectFit: "cover" }}
-          className="responsive-image"
-        />
-      </div>
+   <div className="flex-1/2 relative overflow-hidden">
+  <div className="relative">
+    <Image
+      src={`/Chita/${images[currentImageIndex]}`}
+      alt="Cor da Chita"
+      style={{ maxWidth: "100%", height: "100%", objectFit: "cover" }}
+      className="responsive-image"
+    />
+    <div className="absolute inset-0 flex items-center justify-between">
+  <div className="arrow left text-2xl text-white cursor-pointer z-20" onClick={prevImage}>
+    {"<"}
+  </div>
+  <div className="arrow right text-2xl text-white cursor-pointer z-20" onClick={nextImage}>
+    {">"}
+  </div>
+</div>
+
+  </div>
+</div>
+
       <div className="flex flex-col items-center justify-center text-center z-10 p-20">
         <div >
           <h1 className="text-4xl text-white font-serif">
@@ -30,32 +55,6 @@ export default function Banner() {
           </Link>
         </div>
       </div>
-      <style jsx>{`
-        .image-wrapper {
-          position: relative;
-          width: 100%;
-          height: 0;
-          padding-bottom: 100%;
-        }
-
-        .responsive-image {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-
-        @media (max-width: 640px) {
-          .flex {
-            flex-direction: column;
-          }
-          .responsive-image {
-            height: 100%;
-            width: 100%;
-          }
-        }
-      `}</style>
     </div>
   );
 }
