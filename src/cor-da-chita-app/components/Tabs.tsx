314dd06b0b27a2f8d9e97cf236e575f7c7ee5c19
Julia Sartori <1929sartori@gmail.com>
2023-11-27 12:33:10 +0100

    frases rotacionando

--- src/cor-da-chita-app/components/Tabs.tsx
+++ src/cor-da-chita-app/components/Tabs.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Tabs, Tab } from "@nextui-org/react";
 
 const quotes = [
   {
-    quote: "Norte. A poesia é uma estrela que, farol, me guia e dessa vida-labirinto me abduz pela luz que irradia.",
+    quote:
+      "Norte. A poesia é uma estrela que, farol, me guia e dessa vida-labirinto me abduz pela luz que irradia.",
     author: "Marineuma de Oliveira",
   },
   {
@@ -11,7 +12,8 @@ const quotes = [
     author: "Paulo Coelho",
   },
   {
-    quote: "O correr da vida embrulha tudo, a vida é assim: esquenta e esfria, aperta e daí afrouxa, sossega e depois desinquieta. O que ela quer da gente é coragem.",
+    quote:
+      "O correr da vida embrulha tudo, a vida é assim: esquenta e esfria, aperta e daí afrouxa, sossega e depois desinquieta. O que ela quer da gente é coragem.",
     author: "Guimarães Rosa",
   },
   {
@@ -19,20 +21,44 @@ const quotes = [
     author: "Manoel de Barros",
   },
   {
-    quote: " Como dois e dois são quatro, sei que a vida vale a pena, embora o pão seja caro e a liberdade pequena.",
+    quote:
+      " Como dois e dois são quatro, sei que a vida vale a pena, embora o pão seja caro e a liberdade pequena.",
     author: "Ferreira Gullar",
   },
-  
 ];
 
 export default function QuotesTabs() {
+  const [selectedKey, setSelectedKey] = useState<string>("1");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSelectedKey((prevKey) => {
+        const nextIndex = (parseInt(prevKey) + 1) % quotes.length;
+        return nextIndex === 0 ? quotes.length.toString() : nextIndex.toString();
+      });
+    }, 5000); 
+
+    return () => clearInterval(intervalId); 
+  }, []); 
+
   return (
     <div className="flex flex-col bg-light p-10 items-center">
-      <Tabs aria-label="Options" radius="none" size="lg" variant="underlined" color="default" classNames={{ tabList: "gap-6 w-full relative rounded-none p-0 border-b border-divider" }}>
+      <Tabs
+        aria-label="Options"
+        radius="none"
+        size="lg"
+        variant="underlined"
+        color="default"
+        classNames={{
+          tabList: "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+        }}
+        selectedKey={selectedKey}
+        onSelectionChange={(index) => setSelectedKey(String(index))}
+      >
         {quotes.map((quoteData, index) => (
           <Tab key={index + 1} title={<SquareIcon />} className="flex flex-col items-center">
             <div className="flex-1 text-center p-2">
-              <blockquote className="text-2xl font-light tracking-wide italic p-4 ">
+              <blockquote className="text-2xl font-light tracking-wide italic p-4 font-serif">
                 {quoteData.quote}
               </blockquote>
               <p className="text-medium font-medium text-gray-500 p-2">— {quoteData.author}</p>
@@ -42,8 +68,5 @@ export default function QuotesTabs() {
       </Tabs>
     </div>
   );
-}
-
-const SquareIcon = () => (
-  <div className="m-3 w-10 h-2"></div>
-);
+};
+const SquareIcon = () => <div className="m-3 w-10 h-2"></div>;
