bdbb95985003f9c9b7f6c0a683300a6246d496f6
Julia Sartori <1929sartori@gmail.com>
2023-11-29 19:16:03 +0100

    encomendas ok

--- src/cor-da-chita-app/app/layout.tsx
+++ src/cor-da-chita-app/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
             {children}
 
           </div>
+          <Encomenda />
           <Footer />
         </Providers>
       </body>
--- src/cor-da-chita-app/components/Encomenda.tsx
+++ src/cor-da-chita-app/components/Encomenda.tsx
@@ -1,14 +1,8 @@
-import { Link } from "@nextui-org/react";
-import { MyButton } from "@/components/ui/Button";
+'use Client' 
+import Link from "next/link";
 import FlowerIcon2 from "@/assets/icons/FlowerIcon2";
 
 export default function Encomenda() {
-  const handleContactButtonClick = () => {
-    const emailAddress = 'your-email@example.com';
-    
-    window.location.href = `mailto:${emailAddress}`;
-  };
-
  
   return (
     <div className="bg-green  flex flex-col md:flex-row justify-around place-items-center gap-5 px-4 md:px-20 py-5">
@@ -23,19 +17,12 @@ export default function Encomenda() {
         Faça sua encomenda!
       </h2>
       </div>
-      <MyButton
-        color="transparent"
-        className="px-10 text-light mb-4 md:mb-0 md:mr-4 rounded-full border-2"
-      >
         <Link
-          isExternal
-          className="text-white"
+          className="text-white border rounded-full p-5 hover:scale-105 hover:opacity-80"
           href="https://api.whatsapp.com/send?phone=5583987261972"
-          onClick={handleContactButtonClick}
         >
         Entrar em contato
         </Link>
-      </MyButton>
       <FlowerIcon2  style={{  color: 'white' }} />
       <FlowerIcon2  style={{  color: 'white' }} />
       <FlowerIcon2  style={{  color: 'white' }} />
