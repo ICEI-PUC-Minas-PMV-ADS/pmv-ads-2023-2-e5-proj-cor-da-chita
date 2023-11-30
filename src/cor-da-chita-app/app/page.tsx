54d98ebb1f822576027e36ef851f446fe17695a3
Julia Sartori <1929sartori@gmail.com>
2023-11-30 13:23:09 +0100

    ajeita encomenda na Landing Page

--- src/cor-da-chita-app/app/page.tsx
+++ src/cor-da-chita-app/app/page.tsx
@@ -2,12 +2,11 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
-import { MyButton } from "@/components/ui/Button";
 import Banner from "../components/Banner";
 import LPGrid from "../components/LPGrid";
 import QuotesTabs from "../components/Tabs";
 import InstagramComponent from "../components/InstagramComponent";
-import Encomenda from "@/components/Encomenda";
+import FlowerIcon2 from '@/assets/icons/FlowerIcon2';
 
 
 export default function Home() {
@@ -25,7 +24,26 @@ export default function Home() {
         </Link>
       </div> */}
       <LPGrid />
-      <Encomenda />
+      <div className="bg-green flex flex-col md:flex-row justify-around place-items-center gap-5 px-4 md:px-20 py-5">
+      <FlowerIcon2 style={{ color: 'white' }} />
+      <FlowerIcon2 style={{ color: 'white' }} />
+      <FlowerIcon2 style={{ color: 'white' }} />
+      <div>
+        <h2 className="text-xl text-white font-open">
+          Quer um produto Cor da Chita personalizado?
+        </h2>
+        <h2 className="text-xl text-white">Faça sua encomenda!</h2>
+      </div>
+      <Link
+        className="text-white border rounded-full p-5 hover:scale-105 hover:opacity-80"
+        href="https://api.whatsapp.com/send?phone=5583987261972"
+      >
+        Entrar em contato
+      </Link>
+      <FlowerIcon2 style={{ color: 'white' }} />
+      <FlowerIcon2 style={{ color: 'white' }} />
+      <FlowerIcon2 style={{ color: 'white' }} />
+    </div>
       <InstagramComponent />
     </main>
   );
