5da5d514f93eed001bdc1475d0ee285d5e27f7ec
Julia Sartori <1929sartori@gmail.com>
2023-12-02 15:48:04 +0100

    responsividade instagram ok

---
+++ src/cor-da-chita-app/components/ui/Flowers.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FlowerIcon2 from "@/assets/icons/FlowerIcon2";
+
+function Flowers() {
+  return (
+    <div className="flex flex-row justify-between">
+      <FlowerIcon2 fontSize="60" color="white" strokeWidth="3" />
+      <FlowerIcon2 fontSize="60" color="white" strokeWidth="3" />
+      <FlowerIcon2 fontSize="60" color="white" strokeWidth="3" />
+    </div>
+  );
+}
+
+export default Flowers;
