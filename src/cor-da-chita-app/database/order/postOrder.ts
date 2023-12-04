94b340f90cc13a119103420c1b65d97875641365
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-12-04 20:36:12 -0300

    Trocando url para url daapi da page de criação de pedido

--- src/cor-da-chita-app/database/order/postOrder.ts
+++ src/cor-da-chita-app/database/order/postOrder.ts
@@ -1,8 +1,9 @@
 import { baseURL } from "../baseURL";
+import { url } from "@/app/api/backend/webApiUrl";
 import axios from "axios";
 const postOrder = async (params: any) => {
   console.log(params);
-  console.log(`${baseURL}/api/v1/Order`);
+  console.log(`${url}/api/v1/Order`);
 
   const res = await axios.post(`${baseURL}/api/v1/Order`,params)
     .then((r) => {
